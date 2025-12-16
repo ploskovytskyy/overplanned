8cf34e5962d3e8f989dba443a7f2429f1872e68f
Oleksandr Ploskovytskyy <oleksandr.ploskovytskyy@gmail.com>
2025-12-16 20:11:36 +0900

    fix: enable netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-// import netlify from "@netlify/vite-plugin-tanstack-start";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { devtools } from "@tanstack/devtools-vite";
 
 const config = defineConfig({
@@ -13,7 +13,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    // netlify(),
+    netlify(),
   ],
 });
 
